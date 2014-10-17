95b07b560836597ad5dd034092e667314d55a175
Alexander Berntsen <alexander@plaimi.net>
2014-10-17 15:48:50 +0200

    Add MultiParamTypeClasses
    Only one type is permitted as an instance head following changes in
    Persistent. Add MultiParamTypeClasses to permit us to have more than one
    type. This change is so trivial that it's better to do this than make
    the Persistent dependency more strict.
    
    Signed-off-by: Alexander Berntsen <alexander@plaimi.net>
    Signed-off-by: Stian Ellingsen <stian@plaimi.net>
    Reviewed-by: Stian Ellingsen <stian@plaimi.net>

--- src/Tempuhs/Chronology.hs
+++ src/Tempuhs/Chronology.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE QuasiQuotes                #-}
 {-# LANGUAGE TemplateHaskell            #-}
